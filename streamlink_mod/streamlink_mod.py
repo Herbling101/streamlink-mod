de63408e72496a521b7844354da40c2a649cfb6b
Hunter <84436810+Herbling101@users.noreply.github.com>
2024-06-09 00:44:19 -0700

    Update streamlink_mod.py

--- streamlink_mod/streamlink_mod.py
+++ streamlink_mod/streamlink_mod.py
@@ -16,27 +16,27 @@ def start_streamlink(url, filename):
     )
     streamlink_cmd = f"streamlink {url} best -o {current_filename}"
     try:
-        print(f"Starting streamlink with command: {streamlink_cmd}")
+        print(f"[*_mod*] Starting streamlink with command: {streamlink_cmd}")
         process = subprocess.Popen(streamlink_cmd, shell=True)
         file_counter += 1
     except Exception as e:
-        print(f"Failed to start streamlink: {e}")
+        print(f"[*_mod*] Failed to start streamlink: {e}")
 
 
 def terminate_process_and_children(pid):
     try:
         parent = psutil.Process(pid)
-        print(f"Terminating parent process {pid}")
+        print(f"[*_mod*] Terminating parent process {pid}")
         for child in parent.children(recursive=True):
-            print(f"Terminating child process {child.pid}")
+            print(f"[*_mod*] Terminating child process {child.pid}")
             child.terminate()
         parent.terminate()
         gone, still_alive = psutil.wait_procs([parent], timeout=5)
         for p in still_alive:
-            print(f"Killing unresponsive process {p.pid}")
+            print(f"[*_mod*] Killing unresponsive process {p.pid}")
             p.kill()
     except psutil.NoSuchProcess:
-        print(f"No such process: {pid}")
+        print(f"[*_mod*] No such process: {pid}")
 
 
 def monitor_streamlink(url, filename):
@@ -44,24 +44,24 @@ def monitor_streamlink(url, filename):
     try:
         start_streamlink(url, filename)
         while True:
-            time.sleep(10)  # Check every 10 seconds
+            time.sleep(1)  # Check every 1 seconds
             if process.poll() is not None:  # If the process has terminated
-                print("Streamlink process has stopped. Restarting...")
+                print("[*_mod*] Streamlink process has stopped. Restarting...")
                 start_streamlink(url, filename)
     except KeyboardInterrupt:
         if process:
-            print("Interrupted! Stopping the streamlink process...")
+            print("[*_mod*] Interrupted! Stopping the streamlink process...")
             terminate_process_and_children(process.pid)
         sys.exit(0)
 
 
 def main():
     if len(sys.argv) != 3:
-        print("Usage:\n$streamlink_mod URL FILENAME.mp4\n")
+        print("[*_mod*] Usage:\n\t$ streamlink_mod URL FILENAME.mp4\n")
         sys.exit(1)
     url = sys.argv[1]
     filename = sys.argv[2]
-    print(f"Monitoring stream with URL: {url} and filename: {filename}")
+    print(f"[*_mod*] Monitoring stream with URL: {url} and filename: {filename}")
     monitor_streamlink(url, filename)
 
 
